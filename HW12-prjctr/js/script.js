aa84d188085e75f0fb764938210efde02903b05d
Yevheniia Voitsekhovska <sunakojane6@gmail.com>
2024-03-08 02:22:07 +0200

    homework-HW12

---
+++ HW12-prjctr/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let navigation = document.getElementById("headerTwo");
+console.log(navigation);
+
+let firstElement = document.querySelector(".firstSection");
+console.log(firstElement);
+
+let textElement = document.getElementsByTagName("li")[4];
+console.log(textElement);
+
+let classElement = document.getElementsByClassName("hatredLevelBlock");
+console.log(classElement);
