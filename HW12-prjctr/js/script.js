2f42009216ab39901c82ea9c4035df3aa3d74973
Yevheniia Voitsekhovska <sunakojane6@gmail.com>
2024-03-11 16:37:40 +0200

    homework-HW12

--- HW12-prjctr/js/script.js
+++ HW12-prjctr/js/script.js
@@ -6,8 +6,11 @@ console.log(navigation);
 let firstElement = document.querySelector(".firstSection");
 console.log(firstElement);
 
-let textElement = document.getElementsByTagName("li")[4];
+let textElement = document.querySelector("li:nth-child(5)");
 console.log(textElement);
 
-let classElement = document.getElementsByClassName("hatredLevelBlock");
+let classElement = document.querySelector(".hatredLevelBlock");
 console.log(classElement);
+
+// let classElement = document.getElementsByClassName("hatredLevelBlock")[0];
+// console.log(classElement);
